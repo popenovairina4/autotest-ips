b446041ec460dbd11d8483231139f15f9ab8931e
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2023-09-16 19:44:53 +0300

    Обновил тест

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,6 +1,22 @@
 describe('test describe', () => {
+    before(async () => {
+        // runs once before the first test in this block
+    })
+
+    beforeEach(async () => {
+        // runs before each test in this block
+    });
+
     it('test it', async () => {
         await browser.url(`http://google.com/`)
         await browser.pause(3000)
     })
+
+    afterEach(async () => {
+        // runs after each test in this block
+    });
+
+    after(async () => {
+        // runs once after the last test in this block
+    });
 })
